6e81baffdfce836ac954aa54ce3ccd506a58d371
Young <andreasyoungandersen@gmail.com>
2022-05-19 18:29:21 +0200

    User is informed before running the script

--- Bomber Royale Requeue.bat
+++ Bomber Royale Requeue.bat
@@ -1,2 +1,19 @@
+type info.txt
 
-python requeue.py False
+@ECHO OFF
+
+:choice
+    set /P c="Are you sure you want to continue[Y/N]?"
+    if /I "%c%" EQU "Y" goto :continue_yes
+    if /I "%c%" EQU "N" goto :continue_no
+    goto :choice
+
+
+:continue_yes
+    @ECHO "Bot is turning on. Please ensure Trove is opened within 5 seconds"
+    @ECHO on
+    python requeue.py False
+exit
+
+:continue_no
+exit
